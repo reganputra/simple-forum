40d8c4a5014f6f6727626353c0348602a489425b
reganputra <ramadhanregan334@gmail.com>
2025-08-09 18:10:31 +0700

    Refactor main function to improve configuration handling and database connection

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"simple-forum/internal/configs"
 	"simple-forum/internal/handler/membership"
+	"simple-forum/pkg/internalsql"
+
+	membershipRepo "simple-forum/internal/repository/membership"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql" // Import MySQL driver
@@ -11,6 +14,10 @@ import (
 
 func main() {
 	r := gin.Default()
+
+	var (
+		cfg *configs.Config
+	)
 
 	// Initialize config
 	err := configs.Init(
@@ -23,10 +30,17 @@ func main() {
 	}
 
 	// Get the initialized config
-	cfg := configs.GetConfig()
+	cfg = configs.GetConfig()
 	log.Println("config:", cfg)
 
-	membershipHandler := membership.NewHandler(r, cfg)
+	// Connect to Database
+	db, err := internalsql.Connect(cfg.Database.DataSourceName)
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+	_ = membershipRepo.NewRepository(db)
+
+	membershipHandler := membership.NewHandler(r)
 
 	membershipHandler.PingRoutes()
 
