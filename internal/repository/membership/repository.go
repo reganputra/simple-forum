625638d3f222726ebc2f66a51e6b8ad72f8ebe53
reganputra <ramadhanregan334@gmail.com>
2025-08-10 07:22:50 +0700

    Remove unused logging and user query from repository initialization

--- internal/repository/membership/repository.go
+++ internal/repository/membership/repository.go
@@ -2,7 +2,6 @@ package membership
 
 import (
 	"database/sql"
-	"log"
 )
 
 type repository struct {
@@ -10,24 +9,6 @@ type repository struct {
 }
 
 func NewRepository(db *sql.DB) *repository {
-
-	rows, err := db.Query("SELECT id, email FROM users")
-	if err != nil {
-		return nil
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var id int64
-		var email string
-
-		err = rows.Scan(&id, &email)
-		if err != nil {
-			return nil
-		}
-		log.Printf("User found: %d, %s", id, email)
-	}
-
 	return &repository{
 		db: db,
 	}
