a3b136a5bb3fbe773506d855fa40f2404a616025
reganputra <ramadhanregan334@gmail.com>
2025-08-13 19:30:44 +0700

    Add migration to rename column `post_hastags` to `post_hashtags` in posts table

---
+++ scripts/migrations/000005_rename_post_hastags_to_post_hashtags.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE posts
+    CHANGE COLUMN post_hashtags post_hastags LONGTEXT NOT NULL;
---
+++ scripts/migrations/000005_rename_post_hastags_to_post_hashtags.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE posts
+    CHANGE COLUMN post_hastags post_hashtags LONGTEXT NOT NULL;
